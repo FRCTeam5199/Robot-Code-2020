2caa4a064a65c1dd7beb055c4233e5468a7d0927
Conor O'Brien <30542291+Baygull@users.noreply.github.com>
2020-01-07 21:34:27 -0800

    first push
    created the project and started setting stuff up

---
+++ 2020robot/src/main/java/frc/drive/Driver.java
@@ -0,0 +1,34 @@
+package frc.drive;
+
+import frc.robot.Robot;
+import frc.robot.RobotMap;
+import frc.robot.RobotNumbers;
+
+import frc.controllers.XBoxController;
+
+import java.io.IOException;
+
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANPIDController;
+import com.revrobotics.CANSparkMax.FaultID;
+import com.revrobotics.CANSparkMax.IdleMode;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+import com.revrobotics.ControlType;
+
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+public class Driver{
+    private final XBoxController controller;
+
+    public Driver(){
+        controller = new XBoxController(0);
+    }
+
+    public void init(){
+
+    }
+
+    public void update(){
+
+    }
+}
