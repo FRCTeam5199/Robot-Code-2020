2e600fe3e4e46c0b6b6b321749d1a7a18461be07
Conor O'Brien <30542291+Baygull@users.noreply.github.com>
2020-01-30 10:01:42 -0800

    Commented out old pathfinder code

--- 2020robot/src/main/java/frc/drive/Driver.java
+++ 2020robot/src/main/java/frc/drive/Driver.java
@@ -218,8 +218,7 @@ public class Driver{
     private double adjustedRotation(double input){
         return input*RobotNumbers.maxRotation;
     }
-
-
+    
     private double convertFPStoRPM(double FPS){
         return FPS*(RobotNumbers.maxMotorSpeed/RobotNumbers.maxSpeed);
     }
@@ -463,92 +462,91 @@ public class Driver{
         }
     }
 
+    // private SpeedController m_left_motor;
+    // private SpeedController m_right_motor;
 
-    private SpeedController m_left_motor;
-    private SpeedController m_right_motor;
+    // private Encoder m_left_encoder;
+    // private Encoder m_right_encoder;
 
-    private Encoder m_left_encoder;
-    private Encoder m_right_encoder;
+    // private AnalogGyro m_gyro;
 
-    private AnalogGyro m_gyro;
+    // private EncoderFollower m_left_follower;
+    // private EncoderFollower m_right_follower;
 
-    private EncoderFollower m_left_follower;
-    private EncoderFollower m_right_follower;
+    // private Notifier m_follower_notifier;
 
-    private Notifier m_follower_notifier;
+    // private static int k_ticks_per_rev = 2048;
+    // private static double k_wheel_diameter = 6.0 / 12.0;
+    // private static double k_max_velocity = 8;
 
-    private static int k_ticks_per_rev = 2048;
-    private static double k_wheel_diameter = 6.0 / 12.0;
-    private static double k_max_velocity = 8;
+    // private static int k_left_channel = 0;
+    // private static int k_right_channel = 1;
 
-    private static int k_left_channel = 0;
-    private static int k_right_channel = 1;
+    // private static int k_left_encoder_port_a = 0;
+    // private static int k_left_encoder_port_b = 1;
+    // private static int k_right_encoder_port_a = 2;
+    // private static int k_right_encoder_port_b = 3;
 
-    private static int k_left_encoder_port_a = 0;
-    private static int k_left_encoder_port_b = 1;
-    private static int k_right_encoder_port_a = 2;
-    private static int k_right_encoder_port_b = 3;
+    // private static int k_gyro_port = 0;
 
-    private static int k_gyro_port = 0;
+    // private static String k_path_name = "RunTowardsTrench";
 
-    private static String k_path_name = "RunTowardsTrench";
-
-    /**
-     * run during robot init
-     */
-    public void setupPathfinderAuto(){
-        m_left_encoder = new Encoder(k_left_encoder_port_a, k_left_encoder_port_b);
-        m_right_encoder = new Encoder(k_right_encoder_port_a, k_right_encoder_port_b);
-    }
-    /**
-     * run during auton init
-     */
-    public void initPathfinderAuto(){
-        try {
-            Trajectory left_trajectory = PathfinderFRC.getTrajectory(k_path_name + ".left");
-            Trajectory right_trajectory = PathfinderFRC.getTrajectory(k_path_name + ".right");
+    // /**
+    //  * run during robot init
+    //  */
+    // public void setupPathfinderAuto(){
+    //     m_left_encoder = new Encoder(k_left_encoder_port_a, k_left_encoder_port_b);
+    //     m_right_encoder = new Encoder(k_right_encoder_port_a, k_right_encoder_port_b);
+    // }
+    // /**
+    //  * run during auton init
+    //  */
+    // public void initPathfinderAuto(){
+    //     try {
+    //         Trajectory left_trajectory = PathfinderFRC.getTrajectory(k_path_name + ".left");
+    //         Trajectory right_trajectory = PathfinderFRC.getTrajectory(k_path_name + ".right");
         
-            m_left_follower = new EncoderFollower(left_trajectory);
-            m_right_follower = new EncoderFollower(right_trajectory);
+    //         m_left_follower = new EncoderFollower(left_trajectory);
+    //         m_right_follower = new EncoderFollower(right_trajectory);
         
-            m_left_follower.configureEncoder(m_left_encoder.get(), k_ticks_per_rev, k_wheel_diameter);
-            // You must tune the PID values on the following line!
-            m_left_follower.configurePIDVA(8.0, 0.0, 0.0, 1 / k_max_velocity, 0);
+    //         m_left_follower.configureEncoder(m_left_encoder.get(), k_ticks_per_rev, k_wheel_diameter);
+    //         // You must tune the PID values on the following line!
+    //         m_left_follower.configurePIDVA(8.0, 0.0, 0.0, 1 / k_max_velocity, 0);
         
-            m_right_follower.configureEncoder(m_right_encoder.get(), k_ticks_per_rev, k_wheel_diameter);
-            // You must tune the PID values on the following line!
-            m_right_follower.configurePIDVA(8.0, 0.0, 0.0, 1 / k_max_velocity, 0);
+    //         m_right_follower.configureEncoder(m_right_encoder.get(), k_ticks_per_rev, k_wheel_diameter);
+    //         // You must tune the PID values on the following line!
+    //         m_right_follower.configurePIDVA(8.0, 0.0, 0.0, 1 / k_max_velocity, 0);
         
-            m_follower_notifier = new Notifier(this::followPath);
-            m_follower_notifier.startPeriodic(left_trajectory.get(0).dt);
-          } catch (IOException e) {
-            e.printStackTrace();
-          }
-    }
+    //         m_follower_notifier = new Notifier(this::followPath);
+    //         m_follower_notifier.startPeriodic(left_trajectory.get(0).dt);
+    //       } catch (IOException e) {
+    //         e.printStackTrace();
+    //       }
+    // }
 
-    private void followPath() {
-        if (m_left_follower.isFinished() || m_right_follower.isFinished()) {
-          m_follower_notifier.stop();
-        } else {
-          double left_speed = m_left_follower.calculate(m_left_encoder.get());
-          double right_speed = m_right_follower.calculate(m_right_encoder.get());
-          double heading = yawRel();
-          double desired_heading = Pathfinder.r2d(m_left_follower.getHeading());
-          double heading_difference = Pathfinder.boundHalfDegrees(desired_heading - heading);
-          double turn =  0.8 * (-1.0/80.0) * heading_difference;
-          leaderL.set(left_speed + turn);
-          leaderR.set(right_speed - turn);
-        }
-    }
+    // private void followPath() {
+    //     if (m_left_follower.isFinished() || m_right_follower.isFinished()) {
+    //       m_follower_notifier.stop();
+    //     } else {
+    //       double left_speed = m_left_follower.calculate(m_left_encoder.get());
+    //       double right_speed = m_right_follower.calculate(m_right_encoder.get());
+    //       double heading = yawRel();
+    //       double desired_heading = Pathfinder.r2d(m_left_follower.getHeading());
+    //       double heading_difference = Pathfinder.boundHalfDegrees(desired_heading - heading);
+    //       double turn =  0.8 * (-1.0/80.0) * heading_difference;
+    //       leaderL.set(left_speed + turn);
+    //       leaderR.set(right_speed - turn);
+    //     }
+    // }
 
-    /**
-     * stop, and deactivate ~~robots~~ motors
-     */
-    public void stopMotors(){
-        m_follower_notifier.stop();
-        leaderL.set(0);
-        leaderR.set(0);
-    }
+    // /**
+    //  * stop, and deactivate ~~robots~~ motors
+    //  */
+    // public void stopMotors(){
+    //     m_follower_notifier.stop();
+    //     leaderL.set(0);
+    //     leaderR.set(0);
+    // }
 
     public void closeLogger(){
         logger.close();
--- 2020robot/src/main/java/frc/robot/Robot.java
+++ 2020robot/src/main/java/frc/robot/Robot.java
@@ -88,7 +88,7 @@ public class Robot extends TimedRobot {
 
   @Override
   public void teleopInit() {
-    driver.stopMotors();
+    //driver.stopMotors();
     //shooter.initLogger();
     //pdp.initLogger();
   }
